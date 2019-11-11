cecf39ba6eb0233bec8bd4dd528ea9b91f3df996
Sergii Nevydanchuk <sergii.nevydanchuk@gmail.com>
2019-11-12 01:40:56 +0200

    Counter enhanced.

--- Basic/src/Counter.scala
+++ Basic/src/Counter.scala
@@ -1,12 +1,19 @@
 object Counter extends App {
 
-  val counter = Counter().inc().inc().inc().inc().inc().dec().inc()
+  val counter = Counter().inc(10).inc(10).inc(5).inc(3).inc(2).dec(5).inc(2)
+
+  case class Adder(amount: Int) {
+    def apply(in: Int): Int = in + amount
+  }
 
   case class Counter(value: Int = 0) {
-    def inc(): Counter = this.copy(value + 1)
+    def inc(step: Int = 1): Counter = this.copy(value + step)
 
-    def dec(): Counter = this.copy(value - 1)
+    def dec(step: Int = 1): Counter = this.copy(value - step)
+
+    def adjust(adder: Adder): Counter = this.copy(adder(value))
   }
 
   println(s"Counter ${counter.value}")
+  println(s"Counter with adder => ${Counter(10).adjust(Adder(150))}")
 }
