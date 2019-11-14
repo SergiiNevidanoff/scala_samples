517b27b534a69d9ce0681413af22e48823ab44b2
Sergii Nevydanchuk <sergii.nevydanchuk@gmail.com>
2019-11-14 06:26:12 +0200

    Improved shaping up. To have colors.

--- Basic/src/ShapingUp.scala
+++ Basic/src/ShapingUp.scala
@@ -1,3 +1,5 @@
+import ShapingUp.ColorIntensity
+
 object ShapingUp extends App {
 
   sealed trait Shape {
@@ -20,12 +22,18 @@ object ShapingUp extends App {
     override def area: Double = width * height
   }
 
+  sealed trait Color {
+    def red: ColorIntensity.Value
+
+    def green: ColorIntensity.Value
+
+    def blue: ColorIntensity.Value
+  }
+
   case class Circle(radius: Int) extends Shape {
     override def sides: Int = 0
 
     override def perimeter: Double = 2 * Math.PI * radius
-
-    )
 
     override def area: Double = Math.PI * radius * radius
   }
@@ -33,9 +41,33 @@ object ShapingUp extends App {
   case class Rectangle(width: Int, height: Int) extends Rectangular
 
   case class Square(side: Int) extends Rectangular {
-    override def width: Int = side
-
     override def height: Int = side
+
+    override def width: Int = side
+  }
+
+  object Yellow extends Color {
+    override def red: ColorIntensity.Value = ColorIntensity.Light
+
+    override def green: ColorIntensity.Value = ColorIntensity.Light
+
+    override def blue: ColorIntensity.Value = ColorIntensity.Dark
+  }
+
+  object ColorIntensity extends Enumeration {
+    val Light, Dark = Value
+  }
+
+  object Draw {
+    def apply(shape: Shape, color: Color): String = shape match {
+      case Circle(radius) => s"A ${colorName(color)} circle of radius $radius cm"
+      case Rectangle(width, height) => s"A ${colorName(color)} rectangle of width $width cm and height $height cm"
+      case Square(side) => s"A ${colorName(color)} square with each side of $side cm"
+    }
+
+    private def colorName(color: Color) = {
+      color.getClass.getName
+    }
   }
 
 }
